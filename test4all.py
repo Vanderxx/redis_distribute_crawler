679414f46e5a67b4aaf5cbc7779b6de2683ca04a
Vanderick <398077850@qq.com>
2017-05-17 09:27:56 +0800

    edit gitignore

--- test4all.py
+++ test4all.py
@@ -7,7 +7,7 @@ from scrapy.http.request import Request
 
 if __name__ == '__main__':
     redis_server = redis.StrictRedis(host="192.168.1.76", port=6379, password="279279", db=1)
-    test_request = Request(url="https://www.google.com?pwd=handsome&user=vander", method="GET")
+    # test_request = Request(url="https://www.google.com?pwd=handsome&user=vander", method="GET")
 
     # 这是redis set集合插入操作
     # test_key = "dupefilter:request_seen"
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     #     print "the test request not in"
 
     # 这是redis zset插入操作
-    test_key = "sorted_set: test"
+    # test_key = "sorted_set: test"
     # pairs = {
     #     "https://www.baidu.com": 0,
     #     "https://www.google.com": 0,
@@ -30,9 +30,10 @@ if __name__ == '__main__':
     # }
     # redis_server.zadd(test_key, **pairs)
 
-    pipe = redis_server.pipeline()
-    pipe.multi()
-    pipe.zrange(test_key, 0, 0).zremrangebyrank(test_key, 0, 0)
-    results, count = pipe.execute()
-    print results[0]
-    print count
+    # pipe操作
+    # pipe = redis_server.pipeline()
+    # pipe.multi()
+    # pipe.zrange(test_key, 0, 0).zremrangebyrank(test_key, 0, 0)
+    # results, count = pipe.execute()
+    # print results[0]
+    # print count
